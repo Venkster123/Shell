d319a16fa63b14c80dc08923a2fdd9edbbc7b038
Venkataram Sivaram <veskimo123@gmail.com>
2020-01-23 20:06:06 -0800

    Adding exmaples

--- shell/shell.cpp
+++ shell/shell.cpp
@@ -3,10 +3,14 @@
 
 int main()
 {
+	window stdw(stdscr);
 	//win << "Hello World" << endl;
 	//win("Hello World\n");
 	initscr();
 	printw("Hello World\n");
+	refresh();
+	stdw("hi there");
+	refresh();
 	getch();
 	endwin();
 }
